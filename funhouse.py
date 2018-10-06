90c4fc41df949d86b869e268d25e4fcc0cbbf5c3
Andrew Lobos <andrew@lobos.me>
2018-10-06 10:23:25 -0400

    unfix scaling

--- funhouse.py
+++ funhouse.py
@@ -7,8 +7,5 @@ class PCB:
         self.usb = serial.Serial('/dev/ttyACM0', baudrate=9600)
 
     def set(self, port: int, value: int) -> None:
-        value /= 127
-        value *= 90
-        value += 90
         cmd = pack('Hbb', 1337, port, value)
         self.usb.write(bytes(cmd))
